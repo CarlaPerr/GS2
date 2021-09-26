3c24793b11d0da2723668f9b2f4744b7637069e6
CarlaPerr <perroudcarla@gmail.com>
2021-09-25 22:20:36 -0300

    Ingresa usuario y clave

--- intentosusuario.py
+++ intentosusuario.py
@@ -1,11 +1,26 @@
 #! /usr/bin/python3
 
-# Verificar un usuario y clave si estan correctamente ingresados  
-
-#Determinar si es correcto o no
-
-#Permitir solo 3 intentos 
-
-#Determinar si es correcto o no
+# Verificar un usuario y clave si estan correctamente ingresados
+# Determinar si es correcto o no
+# Permitir solo 3 intentos
+i=0
+while i<3:
+    usuario=input("Ingrese su nombre de usuario ")
+    i=i+1
+    if str(usuario)=="Carla":
+        print("Usuario Correcto")
+        clave=input("Ingrese su clave ")
+        if str(clave)=="ROSARIO":
+            print("Bienvenido Carla")
+            break
+        else:
+            print("Clave Incorrecta")
+            if i==3:
+                print("Intentos agotados")
+                break
+    else:
+        print("Usuario Incorrecto")
+        if i==3:
+           print("Intentos agotados")
 
 
